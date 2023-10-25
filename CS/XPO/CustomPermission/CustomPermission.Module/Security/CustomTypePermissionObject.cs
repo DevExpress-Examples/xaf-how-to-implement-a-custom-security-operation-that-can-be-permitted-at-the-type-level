d17d02cf37179a7bc61481e7a5c784f28610e8a0
andrey <andrey.kozhevnikov@devexpress.com>
2023-10-25 16:36:15 +0400

    init xpo

---
+++ CS/XPO/CustomPermission/CustomPermission.Module/Security/CustomTypePermissionObject.cs
@@ -0,0 +1,27 @@
+﻿using DevExpress.ExpressApp.DC;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl.PermissionPolicy;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomPermission.Module.Security {
+    [XafDisplayName("Type Operation Permissions")]
+    public class CustomTypePermissionObject : PermissionPolicyTypePermissionObject {
+        public CustomTypePermissionObject(Session session)
+            : base(session) {
+        }
+        [XafDisplayName("Export")]
+        public SecurityPermissionState? ExportState {
+            get {
+                return GetPropertyValue<SecurityPermissionState?>(nameof(ExportState));
+            }
+            set {
+                SetPropertyValue(nameof(ExportState), value);
+            }
+        }
+    }
+}
